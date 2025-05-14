bb489e43baaa4b335f74d259d97cc6aa823a77fd
marcin wawer <m.wawer.git@icloud.com>
2025-05-14 20:48:12 +0200

    chore: project setup

---
+++ HelloWorldApp/HelloWorldAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppApp.swift
+//  HelloWorldApp
+//
+//  Created by Marcin Wawer on 14-05-2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
