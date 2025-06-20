cf2abb38ca1a6c4fcbf51c52e0464377f5f0d776
marcin wawer <m.wawer.git@icloud.com>
2025-06-20 18:43:06 +0200

    [UIKit Part 2] feat: task 2

--- uikit-part-2/Autolayout/AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
+++ uikit-part-2/Autolayout/AutolatoutPracticalTasks/AutolatoutPracticalTasks/Tasks/Task2ViewController.swift
@@ -11,8 +11,52 @@ import UIKit
 // The button should be centered horizontally and have a fixed distance from the label.
 // Adjust the layout to handle different screen sizes.
 final class Task2ViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        label.font = .systemFont(ofSize: 36)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let button: UIButton = {
+        var config = UIButton.Configuration.filled()
+        config.title = "Button"
+        config.baseBackgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.cornerStyle = .capsule
+        
+        let button = UIButton(configuration: config)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(label)
+        view.addSubview(button)
+        addConstraints()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+                label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                label.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 10),
+                label.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -10),
+                label.widthAnchor.constraint(lessThanOrEqualToConstant: 500)
+            ])
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.widthAnchor.constraint(equalToConstant: 120)
+        ])
     }
 }
 
