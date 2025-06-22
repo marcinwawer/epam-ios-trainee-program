2879ac40c3250712f67787f4caaf3a6a1c1c1389
marcin wawer <m.wawer.git@icloud.com>
2025-06-22 14:09:52 +0200

    [UIKit Part 4] feat: rest of part 4

--- uikit-part-4/ImageDisplay/ImageDisplay/ViewController.swift
+++ uikit-part-4/ImageDisplay/ImageDisplay/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
     
     private let padding: CGFloat = 20
     
@@ -16,14 +16,14 @@ class ViewController: UIViewController {
         sv.bounces = true
         sv.alwaysBounceHorizontal = true
         sv.alwaysBounceVertical = true
-        sv.minimumZoomScale = 0.4
-        sv.maximumZoomScale = 3.0
+        sv.minimumZoomScale = 0.1
+        sv.maximumZoomScale = 3
         
         return sv
     }()
     
     private let imageView: UIImageView = {
-        let iv = UIImageView(image: UIImage(named: "cat"))
+        let iv = UIImageView(image: UIImage(named: "dots"))
         iv.contentMode = .scaleAspectFill
         
         return iv
@@ -31,14 +31,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupView()
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
-        scrollView.zoomScale = 0.4
+        centerImage()
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
         centerImage()
     }
     
@@ -47,7 +52,6 @@ class ViewController: UIViewController {
         
         view.addSubview(scrollView)
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
         scrollView.addSubview(imageView)
         
         NSLayoutConstraint.activate([
@@ -61,24 +65,45 @@ class ViewController: UIViewController {
     }
     
     private func centerImage() {
-        let boundsSize = scrollView.bounds.size
-        let contentSize = scrollView.contentSize
+        let scrollViewSize = scrollView.bounds.size
+        let imageSize = imageView.bounds.size
         
-        let offsetX = max((contentSize.width - boundsSize.width) / 2, 0)
-        let offsetY = max((contentSize.height - boundsSize.height) / 2, 0)
+        let scaledImageWidth = imageSize.width * scrollView.zoomScale
+        let scaledImageHeight = imageSize.height * scrollView.zoomScale
         
-        scrollView.setContentOffset(CGPoint(x: offsetX, y: offsetY), animated: true)
-    }
-}
-
-// MARK: - ZOOM
-extension ViewController: UIScrollViewDelegate {
-    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
-        return imageView
-    }
-    
-    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
-        centerImage()
+        let horizontalInset = max((scrollViewSize.width - scaledImageWidth) / 2, 0)
+        let verticalInset = max((scrollViewSize.height - scaledImageHeight) / 2, 0)
+        
+        let offsetX: CGFloat
+        if scaledImageWidth > scrollViewSize.width {
+            offsetX = (scaledImageWidth - scrollViewSize.width) / 2
+        } else {
+            offsetX = -horizontalInset
+        }
+        
+        let offsetY: CGFloat
+        if scaledImageHeight > scrollViewSize.height {
+            offsetY = (scaledImageHeight - scrollViewSize.height) / 2
+        } else {
+            offsetY = -verticalInset
+        }
+        
+        let newContentInset = UIEdgeInsets(
+            top: verticalInset,
+            left: horizontalInset,
+            bottom: verticalInset,
+            right: horizontalInset
+        )
+        
+        let newContentOffset = CGPoint(x: offsetX, y: offsetY)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.scrollView.contentInset = newContentInset
+            self.scrollView.contentOffset = newContentOffset
+        }
+        
+//        disabling scrolling on horizontal axis, when whole image fit into the screen but veritcal still allowed
+        scrollView.alwaysBounceHorizontal = scaledImageWidth > scrollViewSize.width
     }
 }
 
