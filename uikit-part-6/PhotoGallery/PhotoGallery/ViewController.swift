4c312a42d82cb3d53a468b29d87eddb09df4a7ba
marcin wawer <m.wawer.git@icloud.com>
2025-06-29 20:58:26 +0200

    [UIKit Part 6] chore: photo gallery project setup

---
+++ uikit-part-6/PhotoGallery/PhotoGallery/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by Marcin Wawer on 29-06-2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
